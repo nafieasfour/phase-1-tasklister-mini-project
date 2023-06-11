d617f7768edea4e048c955d05ea1e36bef7823b0
Nafie Asfour <ph.nafee.asfour@gmail.com>
2023-06-11 15:54:26 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let content = document.querySelector('form')
+  content.addEventListener('submit', (e)=> {
+    e.preventDefault()
+    buildToDo(e.target.new_task_description.value);
+    form.reset()
+  })
+})
+
+
+function buildToDo(todo){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = ' x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector("#list").appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+
+}
